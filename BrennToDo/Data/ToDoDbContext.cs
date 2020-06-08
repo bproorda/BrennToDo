295c3a7be321bfaf54ff0353fe893f44290be706
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-08 14:29:23 -0500

    created dbcontext and downloaded packages

---
+++ BrennToDo/Data/ToDoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrennToDo.Data
+{
+    public class ToDoDbContext : DbContext
+    {
+        public ToDoDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
