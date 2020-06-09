c918de35d358704af8ac61ef5979bb681c400c0f
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-09 16:32:07 -0500

    filled out register action, needs JWT

--- BrennToDo/Controllers/ToDoUsersController.cs
+++ BrennToDo/Controllers/ToDoUsersController.cs
@@ -24,14 +24,29 @@ namespace BrennToDo.Controllers
         [HttpPost("Register")]
         public async Task<ActionResult>  Register(RegisterData data)
         {
-            var newId = new ToDoUser
+            var newUser = new ToDoUser
             {
                 UserName = data.UserName,
                 FirstName = data.FirstName,
                 LastName = data.LastName,
                 Email = data.Email
             };
-            var result = await userManager.CreateAsync()
+            var result = await userManager.CreateAsync(newUser, data.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(new
+                {
+                    message = "Registration failed",
+                    errors = result.Errors,
+                });
+            }
+
+            return Ok(new UserWithToken
+            {
+                UserId = user.Id,
+                Token = userManager.CreateToken(user),
+            });
         }
     }
 }
