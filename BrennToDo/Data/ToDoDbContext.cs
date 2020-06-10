96606834b587a2eca6d44b4fd842958d8bb1704c
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-10 15:28:46 -0500

    add migration and updated database

--- BrennToDo/Data/ToDoDbContext.cs
+++ BrennToDo/Data/ToDoDbContext.cs
@@ -21,16 +21,24 @@ namespace BrennToDo.Data
                 .HasData(
                 new ToDo
                 {
-                    Id  = 1,
+                    Id = 1,
                     Title = "Walk The Dog",
                     Assignee = "Brenn",
                     Difficulty = 3,
                     DueDate = new DateTime(2020, 7, 10, 5, 0, 000, DateTimeKind.Utc)
+                },
+                new ToDo {
+                Id = 2,
+                    Title = "Clean up after Dog",
+                    Assignee = "Graeme",
+                    Difficulty = 5,
+                    DueDate = new DateTime(2020, 7, 10, 5, 0, 000, DateTimeKind.Utc),
+                    CreatedByUserId = "7304a689-e2ff-4b12-a49c-103675becb39"
                 }
                 );
         }
 
-
+        
         public DbSet<BrennToDo.Models.ToDo> ToDo { get; set; }
     }
 }
